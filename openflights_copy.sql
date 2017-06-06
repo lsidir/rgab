21a5d06140c8a74a8bbc3ab3699bc6e891c2a458
Lefteris Sidirourgos <lsidir@gmail.com>
2017-06-06 11:32:10 +0200

    openflights_copy is created by the opnflight-sql.sh script

--- openflights_copy.sql
+++
@@ -1,3 +0,0 @@
-COPY 7184  RECORDS INTO AIRPORTS from '/home/lsidir/monetdb/data/rgab/airports.dat' USING DELIMITERS ',', '\n', '"' NULL AS '\\N'  LOCKED;
-COPY 6162  RECORDS INTO AIRLINES from '/home/lsidir/monetdb/data/rgab/airlines.dat' USING DELIMITERS ',', '\n', '"' NULL AS '\\N'  LOCKED;
-COPY 65612 RECORDS INTO ROUTES   from '/home/lsidir/monetdb/data/rgab/routes.dat'   USING DELIMITERS ',', '\n', '"' NULL AS '\\N'  LOCKED;
