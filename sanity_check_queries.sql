744a7ad5461d5701d14670572ad1a3f3fff49546
Lefteris Sidirourgos <lsidir@gmail.com>
2017-06-08 16:01:18 +0200

    more sanity check queries

--- sanity_check_queries.sql
+++ sanity_check_queries.sql
@@ -76,8 +76,8 @@ SELECT COUNT(*) FROM
 --1 tuple (2.2s)
 
 -- Qs5
--- create a VIEW for the Airlines Graph
-CREATE VIEW airlines_graph_vw AS
+-- create a VIEW for the Airlines Graph (replace MAX() with FIRST() if supported)
+CREATE VIEW airlines_graph_view AS
 SELECT
     airlines1.AirlineID AS AirlineID_src,
     MAX(airlines1.Name) AS Name_src,
@@ -96,3 +96,44 @@ WHERE
     airports.Country = airlines2.Country AND
     airlines2.AirlineID <> routes.AirlineID
 GROUP BY airlines1.AirlineID, airlines2.AirlineID;
+
+-- Qs5a
+-- Count the airlines graph_view
+SELECT COUNT(*) FROM airlines_graph_view;
+--+--------+
+--| L25    |
+--+========+
+--| 467359 |
+--+--------+
+--1 tuple (2.2s)
+
+--Qs5b
+-- Count how many airlines appear as edge sources in the Airlines Graph.
+SELECT COUNT(*) FROM airlines WHERE airlines.AirlineID IN (SELECT AirlineID_src FROM  airlines_graph_view);
+--+------+
+--| L55  |
+--+======+
+--|  544 |
+--+------+
+--1 tuple (2.2s)
+
+--Qs5c
+-- Count how many airlines appear as edge targets in the Airlines Graph.
+SELECT COUNT(*) FROM airlines WHERE airlines.AirlineID IN (SELECT AirlineID_tgt FROM  airlines_graph_view);
+--+------+
+--| L55  |
+--+======+
+--| 5975 |
+--+------+
+--1 tuple (2.3s)
+
+--Qs6
+-- Dense Rank the AirlinesID from table airlines to create unique and consequtive ids for airlines and join them with the source and target of the Airlines graph to upodate the AirlineID_src and AirlineID_tgt to the newly assigned ids from dense rank.
+SELECT src_vertices.id AS src_id,
+       tgt_vertices.id AS tgt_id
+FROM airlines_graph_view,
+     (SELECT airlines.AirlineID, (DENSE_RANK() OVER (ORDER BY airlines.AirlineID)) AS id FROM airlines) AS src_vertices,
+     (SELECT airlines.AirlineID, (DENSE_RANK() OVER (ORDER BY airlines.AirlineID)) AS id FROM airlines) AS tgt_vertices
+WHERE airlines_graph_view.AirlineID_src = src_vertices.AirlineID AND
+      airlines_graph_view.AirlineID_tgt = tgt_vertices.AirlineID;
+
